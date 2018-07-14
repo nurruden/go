7285744adb84329e53f1d16a7f3a7c083518cd1e
allanyang <allanyang@juniper.net>
2018-07-14 09:34:10 +0800

    minor change

--- src/day2/forloop1/main.go
+++ src/day2/forloop1/main.go
@@ -5,9 +5,15 @@ import (
 
 )
 
-func main(){
-	for{
-		fmt.Printf("Hello")
-		break
-	}
-}
+// func main(){
+// 	for{
+// 		fmt.Printf("Hello")
+// 		break
+// 	}
+// }
+
+func main() { 
+	for i := 0;i <= 10; { // initialisation and post are omitted 
+	fmt.Printf("%d\n", i)
+	i += 2
+	} }
